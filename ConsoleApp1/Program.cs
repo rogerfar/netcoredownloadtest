2d207074b643efeb6882c0c0f19f61470fdd5c19
Roger Versluis <roger.versluis@fieldcap.ca>
2021-01-25 07:40:36 -0700

    no message

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sw = new Stopwatch();
+
+            var a = new HttpClient();
+
+            sw.Start();
+            var result = await a.GetByteArrayAsync("https://speed.hetzner.de/100MB.bin");
+            sw.Stop();
+
+            Console.WriteLine($"Downloaded {result.Length} bytes in {sw.Elapsed.TotalSeconds} seconds");
+            Console.ReadKey();
+        }
+    }
+}
